6c4b5af8620ca3d1dfec14c15789e86bc993edec
lilulab <lilu12@andrew.cmu.edu>
2015-10-22 00:34:18 -0400

    Save data as 2D array, [channel_id][pattern_timeStamp].

---
+++ printCppBracket.m
@@ -0,0 +1,33 @@
+function [ data_len ] = printCppBracket( file_id, data_src, data_type, bracket_L, bracket_R)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+% get size of the data set
+data_len = size(data_src);
+
+    
+    % print bracket
+    fprintf(file_id, [bracket_L ' ']);
+
+    % print C array
+    if strcmp(data_type,'float')
+        for i=1:data_len(1)-1
+            % element 1 to n-1
+            fprintf(file_id, '%.2f,\t', data_src(i));
+        end      
+    elseif strcmp(data_type,'uint8_t')
+        for i=1:data_len(1)-1
+            % element 1 to n-1
+            fprintf(file_id, '%d,\t', data_src(i));
+            if data_src(i) < 100;
+                fprintf(file_id, '\t');
+            end
+        end  
+    end
+
+
+    % print the last element in the array
+    fprintf(file_id, ['%d ' bracket_R], data_src(data_len(1)));
+
+end
+
